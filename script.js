469df57724697fce879cf50afbe85ea1d211506d
adit-pal.singh <adit-pal.singh@telekom.de>
2023-05-29 14:38:39 +0200

    adding new files for planet and facts

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* Script for bar menu ==>>> */
 const toggleButton = document.querySelector(".toggle");
 const toggleButtonIcon = document.querySelector(".toggle i");
-const burgerMenu = document.querySelector(".burgerMenu");
+const dropDown = document.querySelector(".dropDown");
 
 toggleButton.onclick = function() {
-    burgerMenu.classList.toggle("open")
-    const isOpen = burgerMenu.classList.contains("open")
+    dropDown.classList.toggle("open")
+    const isOpen = dropDown.classList.contains("open")
     toggleButtonIcon.classList = isOpen
     ? "fa-solid fa-xmark"
     : "fa-solid fa-bars"

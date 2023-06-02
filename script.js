6756284ecafe68fdf8090b8bcb4fb4504b4abcd6
adit-pal.singh <adit-pal.singh@telekom.de>
2023-06-02 15:16:18 +0200

    added new files for crew

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 /* Script for bar menu ==>>> */
-const toggleButton = document.querySelector(".toggle");
+/* const toggleButton = document.querySelector(".toggle");
 const toggleButtonIcon = document.querySelector(".toggle i");
 const dropDown = document.querySelector(".dropDown");
 
-toggleButton.onclick = function() {
+toggleButton.onclick = function () {
     dropDown.classList.toggle("open")
     const isOpen = dropDown.classList.contains("open")
     toggleButtonIcon.classList = isOpen
-    ? "fa-solid fa-xmark"
-    : "fa-solid fa-bars"
+        ? "fa-solid fa-xmark"
+        : "fa-solid fa-bars"
 }
+ */

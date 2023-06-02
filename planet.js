98090b1a540906576e10814efe487c698a31f116
adit-pal.singh <adit-pal.singh@telekom.de>
2023-06-02 09:56:22 +0200

    added file gitignore

--- planet.js
+++ planet.js
@@ -2,17 +2,17 @@
 
 let activeIndex = 0;
 
-const slides = document.getElementsByTagName("article");
+const slides = document.getElementsByTagName("section");
 
 const handleLeftClick = () => {
   const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
   const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
-        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
-        
+    nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+
   currentSlide.dataset.status = "after";
-  
+
   nextSlide.dataset.status = "becoming-active-from-before";
-  
+
   setTimeout(() => {
     nextSlide.dataset.status = "active";
     activeIndex = nextIndex;
@@ -21,14 +21,14 @@ const handleLeftClick = () => {
 
 const handleRightClick = () => {
   const nextIndex = activeIndex + 1 <= slides.length - 1 ? activeIndex + 1 : 0;
-  
+
   const currentSlide = document.querySelector(`[data-index="${activeIndex}"]`),
-        nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
-  
+    nextSlide = document.querySelector(`[data-index="${nextIndex}"]`);
+
   currentSlide.dataset.status = "before";
-  
+
   nextSlide.dataset.status = "becoming-active-from-after";
-  
+
   setTimeout(() => {
     nextSlide.dataset.status = "active";
     activeIndex = nextIndex;

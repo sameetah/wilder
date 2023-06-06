385dcae8ce682bbfecf7466b95c5f317770edd89
adit-pal.singh <adit-pal.singh@telekom.de>
2023-06-06 14:37:51 +0200

    Made the website responsive and added drop down menu

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-/* Script for bar menu ==>>> */
-/* const toggleButton = document.querySelector(".toggle");
-const toggleButtonIcon = document.querySelector(".toggle i");
-const dropDown = document.querySelector(".dropDown");
+/* Script for dropdown menu ==>>> */
+const toggleButton = document.querySelector("#menu");
+const toggleButtonIcon = document.querySelector("#menu i");
+const dropDown = document.querySelector(".dropdown");
 
 toggleButton.onclick = function () {
     dropDown.classList.toggle("open")
     const isOpen = dropDown.classList.contains("open")
-    toggleButtonIcon.classList = isOpen
-        ? "fa-solid fa-xmark"
-        : "fa-solid fa-bars"
+    toggleButtonIcon.classList = isOpen ? "bx bx-x" : "bx bx-menu"
 }
- */

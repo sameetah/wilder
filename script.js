ab00085b187579660132e1b764216821374a2db5
David Alexander Pfeiffer <david.pfeiffer971@gmail.com>
2023-06-07 10:30:20 +0200

    Fixed menu issue by adding safety check

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-{ //! Deprecated: migrate to version below!
+{
+  //! Deprecated: migrate to version below!
   const toggleButton = document.querySelector(".toggle");
   const toggleButtonIcon = document.querySelector(".toggle i");
   const burgerMenu = document.querySelector(".burgerMenu");
 
-  toggleButton.onclick = function () {
-    burgerMenu.classList.toggle("open");
-    const isOpen = burgerMenu.classList.contains("open");
-    toggleButtonIcon.classList = isOpen
-      ? "fa-solid fa-xmark"
-      : "fa-solid fa-bars";
-  };
+  if (toggleButton != null) {
+    toggleButton.onclick = function () {
+      burgerMenu.classList.toggle("open");
+      const isOpen = burgerMenu.classList.contains("open");
+      toggleButtonIcon.classList = isOpen
+        ? "fa-solid fa-xmark"
+        : "fa-solid fa-bars";
+    };
+  }
 }
 
-{ //! New version for menu
+{
+  //! New version for menu
   /* Script for dropdown menu ==>>> */
   const toggleButton = document.querySelector("#menu");
   const toggleButtonIcon = document.querySelector("#menu i");
   const dropDown = document.querySelector(".dropdown");
 
-  toggleButton.onclick = function () {
-    dropDown.classList.toggle("open");
-    const isOpen = dropDown.classList.contains("open");
-    toggleButtonIcon.classList = isOpen ? "bx bx-x" : "bx bx-menu";
-  };
+  if (toggleButton != null) {
+    toggleButton.onclick = function () {
+      dropDown.classList.toggle("open");
+      const isOpen = dropDown.classList.contains("open");
+      toggleButtonIcon.classList = isOpen ? "bx bx-x" : "bx bx-menu";
+    };
+  }
 }
 
 //Sameetah part where she is writing a code where when you click on name
